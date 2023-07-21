0b6d857bb9a77b3ad0bf4e6fdfa2a2e503f666d0
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-21 12:37:03 -0500

    asfasf

--- src/components/ai.tsx
+++ src/components/ai.tsx
@@ -1,7 +1,7 @@
 export default function AI() {
     return (
         <div className="w-[44vw] max-w-[44vw] m-3 ml-0 border bg-primary border-white border-opacity-25 rounded-md">
-            <div className=" bg-gradient-to-r from-secondary to-tertiary flex flex-row  justify-between rounded-t-md border-b border-white border-opacity-25">
+            <div className=" bg-gradient-to-r from-secondary to-tertiary flex flex-row  justify-between rounded-t-md">
                 <h1 className="text-black text-center text-xl flex mt-auto mb-auto ml-4">AI</h1>
                 <div className="text-black flex flex-row">
                     <button className="aibutton p-3 border-l border-black border-opacity-25 h-full w-[6rem]">Context</button>
