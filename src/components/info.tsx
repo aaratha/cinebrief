569fd870b072ede04507f0dc855975e89917c91d
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-21 12:29:34 -0500

    ai buttons

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -36,11 +36,13 @@ export default function Info({movieId}: InfoProps) {
     return (
         <div className=" w-[44vw] max-w-[44vw] m-3 ml-6 bg-primary border border-white border-opacity-25 rounded-md justify-between flex flex-col">
             <div className="flex flex-col">
-                <h1 className="text-center mt-4 text-xl border border-white border-opacity-25 rounded-md p-2 max-w-full m-4 mb-0 bg-gradient-to-r from-secondary to-tertiary">Movie Information</h1>
+                <h1 className="text-center mt-4 text-xl border border-white border-opacity-25 rounded-md p-2 max-w-full m-4 mb-0 bg-gradient-to-r from-secondary to-tertiary text-black">Movie Information</h1>
+                {/*<p>{creator}</p>
+                <p>{roles}</p>*/}
                 <p className="m-7 mt-6">{info}</p>
             </div>
             <div className="m-4 mt-0">
-                <h1 className=" rounded-t-md w-[6rem] text-center p-1 border-b-0 bg-secondary">Top Cast</h1>
+                <h1 className="text-black rounded-t-md w-[6rem] text-center p-1 border-b-0 bg-secondary">Top Cast</h1>
                 <div className="p-[2px] bg-gradient-to-r from-secondary to-tertiary rounded-md rounded-tl-none">
                     <div className="h-[11rem] rounded-md bg-black flex flex-row overflow-x-scroll overflow-y-hidden p-3">
                         {cast.map((actor: string, index: number) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
       },
       colors: {
         primary: '#171717',
-        secondary: '#4108c4',
-        tertiary: '#3730ff',
+        secondary: '#b4b1ff',
+        tertiary: '#a88bff',
       }
     },
   },
